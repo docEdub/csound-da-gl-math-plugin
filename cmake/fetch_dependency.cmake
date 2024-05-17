b189ab8e12465533c25f8fac75e8a64bcc9e7a9c
Andy Fillebrown <andy@docEdub.com>
2024-05-17 04:28:02 -0400

    Add CMake fetch_dependency args `GIT_REPOSITORY` and `GIT_TAG`

--- cmake/fetch_dependency.cmake
+++ cmake/fetch_dependency.cmake
@@ -3,17 +3,36 @@ include_guard()
 include(FetchContent)
 
 macro(fetch_dependency)
-    cmake_parse_arguments(ARG "" "NAME;URL;URL_MD5" "" ${ARGN})
+    cmake_parse_arguments(ARG "" "NAME;GIT_REPOSITORY;GIT_TAG;URL;URL_MD5" "" ${ARGN})
 
     message(STATUS "Fetching ${ARG_NAME}")
 
-    FetchContent_Declare(
-        ${ARG_NAME}
-        URL ${ARG_URL}
-        URL_MD5 ${ARG_URL_MD5}
-        DOWNLOAD_DIR "${FETCHCONTENT_BASE_DIR}/${ARG_NAME}"
-        DOWNLOAD_EXTRACT_TIMESTAMP true
-    )
+    if(ARG_GIT_REPOSITORY)
+        if(NOT ARG_GIT_TAG)
+            message(FATAL_ERROR "GIT_TAG must be provided")
+        endif()
+
+        FetchContent_Declare(
+            ${ARG_NAME}
+            GIT_REPOSITORY ${ARG_GIT_REPOSITORY}
+            GIT_TAG ${ARG_GIT_TAG}
+        )
+    elseif(ARG_URL)
+        if(NOT ARG_URL_MD5)
+            message(FATAL_ERROR "URL_MD5 must be provided")
+        endif()
+
+        FetchContent_Declare(
+            ${ARG_NAME}
+            URL ${ARG_URL}
+            URL_MD5 ${ARG_URL_MD5}
+            DOWNLOAD_DIR "${FETCHCONTENT_BASE_DIR}/${ARG_NAME}"
+            DOWNLOAD_EXTRACT_TIMESTAMP true
+        )
+    else()
+        message(FATAL_ERROR "Either GIT_REPOSITORY or URL must be provided")
+    endif()
+
     FetchContent_MakeAvailable(${ARG_NAME})
 
     message(STATUS "Fetching ${ARG_NAME} - done")
